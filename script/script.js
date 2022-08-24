88334c806f01c4f5a53f1d187e0c180dddecffd9
stephenoduntan <stephenoduntan28@gmail.com>
2022-08-24 04:26:01 +0100

    contact email

--- script/script.js
+++ script/script.js
@@ -5,7 +5,9 @@ const conSect = document.querySelectorAll('.contents section');
 const workNavItem = document.querySelectorAll('.work-nav-link');
 const workCatBlock = document.querySelectorAll('.works');
 const workImage = document.querySelectorAll('.work-image');
-const imgGroup = document.querySelectorAll('.work-image .img-group')
+const imgGroup = document.querySelectorAll('.work-image .img-group');
+const contactBtn = document.querySelector('#contact-btn');
+
 
 //Page header
 document.addEventListener('scroll', function(){
@@ -62,40 +64,26 @@ menuBtn.addEventListener('click', function(){
     }
 })
 
+//contact error/success message
 let successPop = document.querySelector('.success');
 let errorPop = document.querySelector('.error');
 
 function popup(status){
     status.style.display = "flex";
-    setTimeout(() => {
-        status.style.display = "none";
-    }, 3000);
+    // setTimeout(() => {
+    //     status.style.display = "none";
+    // }, 3000);
 }
 
-window.addEventListener("load", function(){
-    function sendData(){
-        const XHR = new XMLHttpRequest();
+//Email
+let emailSubject = document.querySelector('#subject');
+let emailBody = document.querySelector('#message');
+let emailPhone = document.querySelector('#phone');
 
-        const FD = new FormData(contactForm);
-
-        XHR.addEventListener("load", function(){
-            popup(successPop)
-        });
-
-        XHR.addEventListener("error", function(){
-            popup(errorPop)
-        });
-
-        XHR.open("POST", "mailto:stephenoduntan28@gmail.com");
-
-        XHR.send(FD);
+contactBtn.addEventListener("click", function(){
+    if(!window.navigator.onLine){
+        popup(errorPop)
+    }else{
+        window.open(`mailto:stephenoduntan28@gmail.com?subject=${emailSubject.value}&body=${emailBody.value}. You can reach me on ${emailPhone.value} to discuss more`);
     }
-
-    const contactForm = document.querySelector(".contact-form");
-
-    contactForm.addEventListener("submit", function(event){
-        event.preventDefault();
-
-        sendData();
-    })
 })
